810c73733baa6f03cf5223743fa18a2c67b39d8c
mitsuhashi0921 <mitsuhashi0921@gmail.com>
2019-10-26 08:29:40 +0000

    デプロイ前最終版

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,9 @@ class Trip < ApplicationRecord
 	belongs_to :enduser
 	has_many :days, dependent: :destroy, inverse_of: :trip
 	accepts_nested_attributes_for :days, allow_destroy: true
-    validates :title, presence: true
+    validates :title,    length: { in: 1..50 }        # 「1文字以上50文字以下」
+    validates :subtitle,    length: { maximum: 250 }      # 「250文字以下」
+    validates :comment,    length: { maximum: 250 }      # 「250文字以下」
 	has_many :likes, dependent: :destroy
 	def enduser
       return Enduser.find_by(id: self.enduser_id)
